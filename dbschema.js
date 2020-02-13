5989c866d70e774e1079fbafb3954af140a8896c
Janin Grandne <janin.grandne@gmail.com>
2020-02-13 19:39:30 +0100

    first push backend

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+    screams: [
+        {
+            userHandle: 'user',
+            body: 'scream body',
+            createdAt: '2020-02-13T17:28:20.162Z',
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+}
