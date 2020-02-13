9e1f7eb53372f9bef7583a79bacfb728c8ca8f2f
Janin Grandne <47568974+dabizi@users.noreply.github.com>
2020-02-14 00:10:32 +0100

    Added tokenAccess, Route Protected

--- functions/index.js
+++ functions/index.js
@@ -15,6 +15,7 @@ const config = {
     measurementId: "*"
 };
 
+
 const firebase = require('firebase');
 firebase.initializeApp(config);
 //firebase.analytics(); TODO find why analytics don't work
@@ -42,13 +43,43 @@ app.get('/screams', (req, res) => {
     .catch(err => console.error(err));
 })
 
-app.post('/screams', (req, res) => {
+const FBAuth = (req, res, next) => {
+    let idToken;
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')){
+        idToken = req.headers.authorization.split('Bearer ')[1];
+    } else {
+        console.error('No token found');
+        return res.status(403).json({ error: 'Unauthorized'});
+    }
+
+    admin.auth().verifyIdToken(idToken)
+    .then(decodedToken => {
+        req.user = decodedToken;
+        console.log(decodedToken);
+        return db.collection('users')
+            .where('userId', '==', req.user.uid)
+            .limit(1)
+            .get();
+    })
+    .then(data => {
+        req.user.handle = data.docs[0].data().handle;
+        return next();
+    })
+    .catch(err => {
+        console.error('Error while verifying token ', err);
+        return res.status(403).json(err);
+    })
+}
+
+app.post('/screams', FBAuth, (req, res) => {
      if(req.method !== 'POST'){
          return res.status(400).json({ error: 'Method not allowed' });
-     }
+     } else if (req.body.body.trim() === ''){
+        return res.status(400).json({ body: 'Body must not be empty' });
+    }
     const newScream = {
         body: req.body.body,
-        userHandle: req.body.userHandle,
+        userHandle: req.user.handle,
         createdAt: new Date().toISOString()
     };
 
@@ -64,6 +95,17 @@ app.post('/screams', (req, res) => {
         });
  });
 
+ const isEmail = (email) => {
+    const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (email.match(emailRegEx)) return true;
+    else return false;
+}
+
+ const isEmpty = (string) => {
+     if (string.trim() === '') return true;
+     else return false;
+ }
+
 //Signup route
 app.post('/signup', (req, res) => {
     const newUser = {
@@ -72,6 +114,20 @@ app.post('/signup', (req, res) => {
         confirmPassword: req.body.confirmPassword,
         handle: req.body.handle
     };
+
+    let errors = {};
+
+    if (isEmpty(newUser.email)) {
+        errors.email = 'Must not be empty'
+    } else if (!isEmail(newUser.email)){
+        errors.email = 'Must be a valid email address'
+    }
+    if (isEmpty(newUser.password)) errors.password = 'Must not be empty'
+    if (newUser.password !== newUser.confirmPassword) errors.confirmPassword = 'Passwords must be the same';
+    if (isEmpty(newUser.handle)) errors.handle = 'Must not be empty'
+
+    if (Object.keys(errors).length > 0) return res.status(400).json(errors);
+
 
     //TODO : Validate data
     let token, userId;
@@ -113,5 +169,33 @@ app.post('/signup', (req, res) => {
     });
     
 });
+
+app.post('/login', (req, res) => {
+    const user = {
+        email: req.body.email,
+        password: req.body.password
+    };
+
+    let errors = {};
+
+    if (isEmpty(user.email)) errors.email = 'Must not be empty';
+    if (isEmpty(user.password)) errors.password = 'Must not be empty';
+
+    if (Object.keys(errors).length > 0) return res.status(400).json(errors);
+
+    firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+    .then(data => {
+        return data.user.getIdToken();
+    })
+    .then(token => {
+        return res.json({token});
+    })
+    .catch(err => {
+        console.error(err);
+        if (err.code === 'auth/wrong-password'){
+            return res.status(403).json({general: 'Wrong credentials, please try again'});
+        } else return res.status(500).json({error: err.code });
+    })
+})
 
 exports.api = functions.region('europe-west1').https.onRequest(app);
