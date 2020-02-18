e7f911d1b83582168d5b244ef50ff6804dba7434
Janin Grandne <47568974+dabizi@users.noreply.github.com>
2020-02-18 18:35:12 +0100

    Add files via upload

---
+++ functions/utility/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyDLK1eoLVE3pP1OE18m4UgDNl15DMnBwKI",
+    authDomain: "remembweb.firebaseapp.com",
+    databaseURL: "https://remembweb.firebaseio.com",
+    projectId: "remembweb",
+    storageBucket: "remembweb.appspot.com",
+    messagingSenderId: "966289022424",
+    appId: "1:966289022424:web:eff812a971e423a2f526ad",
+    measurementId: "G-FTKTQ1J5TN"
+};
