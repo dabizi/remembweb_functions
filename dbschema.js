18f1eac4284a0380ad8b835be8f9ca0f9eeae50b
Janin Grandne <47568974+dabizi@users.noreply.github.com>
2020-02-18 18:34:36 +0100

    Add files via upload

--- dbschema.js
+++ dbschema.js
@@ -1,4 +1,16 @@
 let db = {
+    users: [
+        {
+            userId: 'dhwjkjlci233332',
+            email: 'jg@gmail.com',
+            handle: 'user',
+            createdAt: '2020-02-13T17:28:20.162Z',
+            imageUrl: 'image/dsakdlj/djskaldj',
+            bio: 'Hello, coucou',
+            website: 'https://user.com',
+            location: 'Paris, FR'
+        }
+    ],
     screams: [
         {
             userHandle: 'user',
@@ -6,6 +18,30 @@ let db = {
             createdAt: '2020-02-13T17:28:20.162Z',
             likeCount: 5,
             commentCount: 2
+        }
+    ]
+};
+
+const userDetails = {
+    //Redux data
+    credentials: {
+            userId: 'dhwjkjlci233332',
+            email: 'jg@gmail.com',
+            handle: 'user',
+            createdAt: '2020-02-13T17:28:20.162Z',
+            imageUrl: 'image/dsakdlj/djskaldj',
+            bio: 'Hello, coucou',
+            website: 'https://user.com',
+            location: 'Paris, FR'
+    },
+    likes : [
+        {
+            userHandle: 'user',
+            screamId: 'jhhhh2hhhhh2h3h4234'
+        },
+        {
+            userHandle: 'user',
+            screamId: 'h3shad4sfgjwelkj234'
         }
     ]
 }
