f9f9cd2c7641b3a992a632568830232237336e37
Janin Grandne <47568974+dabizi@users.noreply.github.com>
2020-02-20 21:02:59 +0100

    Update users.js

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -58,7 +58,7 @@ exports.signup = (req, res) => {
         if (err.code === "auth/email-already-in-use") {
             return res.status(400).json({ email: 'Email is already in use'});
         } else {
-            return res.status(500).json({ error: err.code });
+            return res.status(500).json({ general: 'Something went wrong, please try again' });
         }
     });
     
@@ -108,6 +108,43 @@ exports.addUserDetails = (req, res) => {
     })
 }
 
+exports.getUserDetails = (req, res) => {
+    let userData = {};
+    db.doc(`/users/${req.params.handle}`)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          userData.user = doc.data();
+          return db
+            .collection('screams')
+            .where('userHandle', '==', req.params.handle)
+            .orderBy('createdAt', 'desc')
+            .get();
+        } else {
+          return res.status(404).json({ errror: 'User not found' });
+        }
+      })
+      .then((data) => {
+        userData.screams = [];
+        data.forEach((doc) => {
+          userData.screams.push({
+            body: doc.data().body,
+            createdAt: doc.data().createdAt,
+            userHandle: doc.data().userHandle,
+            userImage: doc.data().userImage,
+            likeCount: doc.data().likeCount,
+            commentCount: doc.data().commentCount,
+            screamId: doc.id
+          });
+        });
+        return res.json(userData);
+      })
+      .catch((err) => {
+        console.error(err);
+        return res.status(500).json({ error: err.code });
+      });
+  };
+
 // Get own user details
 exports.getAuthenticatedUser = (req, res) => {
     let userData = {};
@@ -123,6 +160,22 @@ exports.getAuthenticatedUser = (req, res) => {
         userData.likes = [];
         data.forEach(doc => {
             userData.likes.push(doc.data());
+        });
+        return db.collection('notifications').where('recipient', '==', req.user.handle)
+        .orderBy('createdAt', 'desc').limit(10).get();
+    })
+    .then(data => {
+        userData.notifications = [];
+        data.forEach(doc => {
+            userData.notifications.push({
+                recipient: doc.data().recipient,
+                sender: doc.data().sender,
+                createdAt: doc.data().createdAt,
+                screamId: doc.data().screamId,
+                type: doc.data().type,
+                read: doc.data().read,
+                notificationId: doc.id
+            })
         });
         return res.json(userData);
     })
@@ -180,4 +233,20 @@ exports.uploadImage = (req, res) => {
         });
     });
     busboy.end(req.rawBody);
+};
+
+exports.markNotificationsRead = (req, res) => {
+    let batch = db.batch();
+    req.body.forEach(notificationId => {
+        const notification = db.doc(`/notifications/${notificationId}`);
+        batch.update(notification, { read: true });
+    });
+    batch.commit()
+    .then(() => {
+        return res.json({ message: 'Notifications marked read'});
+    })
+    .catch((err) => {
+        console.error(err);
+        return res.status(500).json({ error: err.code });
+    });
 };
