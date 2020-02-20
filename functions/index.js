219fcfcee0a792778c7a4af016981cb64d808618
Janin Grandne <47568974+dabizi@users.noreply.github.com>
2020-02-20 17:51:07 +0100

    like, unlike and delete route added

--- functions/index.js
+++ functions/index.js
@@ -4,7 +4,16 @@ const app = require('express')();
 
 const FBAuth = require('./utility/fbAuth');
 
-const { getAllScreams, postOneScream } = require('./handlers/screams');
+const { 
+    getAllScreams, 
+    postOneScream, 
+    getScream, 
+    commentOnScream,
+    likeScream,
+    unlikeScream,
+    deleteScream
+} = require('./handlers/screams');
+
 const { 
     signup, 
     login, 
@@ -14,8 +23,13 @@ const {
  } = require('./handlers/users');
 
 //Scream route
-app.get('/screams', getAllScreams);
 app.post('/screams', FBAuth, postOneScream);
+app.post('/screams/:screamId/comment', FBAuth, commentOnScream);
+app.get('/screams', getAllScreams);
+app.get('/screams/:screamId', getScream);
+app.get('/screams/:screamId/like', FBAuth, likeScream);
+app.get('/screams/:screamId/unlike', FBAuth, unlikeScream);
+app.delete('/screams/:screamId', FBAuth, deleteScream)
 
 //Users route
 app.post('/signup', signup);
